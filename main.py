a521771298197316728caa9bf025ecc7734e8b05
rudicodera <rudimar.althof@lowcodera.com>
2024-08-06 01:00:46 +0100

    test api

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import flet as ft
+import os
+
+#supabase8836#
+print(os.getenv("VIRTUAL_ENV"))
+print(os.environ.get("VIRTUAL_ENV"))
+print(os.getenv("API"))
+print(os.environ.get("API"))
+
+api = os.getenv("API") or os.environ.get("API") or "no API set"
 
 
 def main(page: ft.Page):
     page.title = "Rudimar's App"
     page.add(ft.Text("Rudimar Althof's Site - dev"))
+    print("API: ", api)
+    page.add(ft.Text("API" + api))
 
 
 ft.app(main)
