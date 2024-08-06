33c7d0f5386cda0be608c574a09e7718d8e603d3
rudicodera <rudimar.althof@lowcodera.com>
2024-08-06 17:36:54 +0100

    test VAR3

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
 import flet as ft
 import os
+import json
 
-#supabase8836#
-print(os.getenv("VIRTUAL_ENV"))
-print(os.environ.get("VIRTUAL_ENV"))
-print(os.getenv("API"))
-print(os.environ.get("API"))
-print(os.environ)
-
+def on_fetch(request):
+    error = json.dumps({ "error": "The `cf` object is not available inside the preview." })
+    data = request.cf if request.cf is not None else error
+    print("on fetch")
+    print(request)
+    print(data)
+    #headers = Headers.new({"content-type":"application/json"}.items())
+    #return Response.new(JSON.stringify(data, None, 2), headers=headers)
 
 api = os.getenv("API") or os.environ.get("API") or "no API set"
-
 
 def main(page: ft.Page):
     page.title = "Rudimar's App"
@@ -20,4 +20,11 @@ def main(page: ft.Page):
     page.add(ft.Text("API: " + api))
 
 
-ft.app(main)
+#supabase8836#
+print(os.getenv("VIRTUAL_ENV"))
+print(os.environ.get("VIRTUAL_ENV"))
+print(os.getenv("API"))
+print(os.environ.get("API"))
+#print(os.environ)
+
+#ft.app(main)
