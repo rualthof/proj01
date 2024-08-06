5aa8331da8ee9d27d403531f0e3d5b636e526d3a
rudicodera <rudimar.althof@lowcodera.com>
2024-08-06 18:18:33 +0100

    test VAR11

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import flet as ft
 import os
+from cfkv import KVStore
+
+#store = KVStore(namespace_id="1b73c708fab441abad9ca199aca6c5db", account_id="66e8de2e2d9662d21afea4ea9389a0f2", api_key="fKICUL8p6XvAqxcH3XXeWAjszZxDkDJQAoAjsdzw")
+store = KVStore(namespace_id="1b73c708fab441abad9ca199aca6c5db", account_id="66e8de2e2d9662d21afea4ea9389a0f2", api_key="v1.0-dcd760b9a724ce811e593663-d2476aada983ec9c94883a34cfeee139706d6cd4b1226fe46600dfe040e3e9648c792759309ed9b05cdf2ebb972505b34c344f946047b3b10d22e93bd9ca3b7d3576a87c1eef721ecf")
+print(store)
+key = "API"
+get = store.get(key)
+print("KV value: ", get)
 
 api = os.getenv("API") or os.environ.get("API") or "no API set"
 
